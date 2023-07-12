cbb32f8937c4e879b3d3139f3b1c85c5879fef79
Christian Brintnall <christianbrintnall@gmail.com>
2023-07-11 20:36:22 -0700

    modify health events for accuracy

--- generic/health.gd
+++ generic/health.gd
@@ -9,6 +9,12 @@ class HealthChangeRequest:
     owner = _owner
     amount = _amount
 
+class HealthChangedResult:
+  var request: HealthChangeRequest
+  var before: int
+  var after: int
+  var amount_modified: int
+
 signal healed(amount, current)
 signal hurt(amount, current)
 signal died()
@@ -28,10 +34,17 @@ func modify(request: HealthChangeRequest):
   var previous = current_health.current
   current_health.incr(-request.amount)
 
+  var result = HealthChangedResult.new()
+
+  result.request = request
+  result.before = previous
+  result.after = current_health.current
+  result.amount_modified = previous-current_health.current
+
   if previous < current_health.current:
-    healed.emit(request.amount, current_health.current)
+    healed.emit(result)
   elif previous > current_health.current:
-    hurt.emit(request.amount, current_health.current)
+    hurt.emit(result)
 
   if not dead and current_health.current <= 0:
     dead = true
