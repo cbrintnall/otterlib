013dfcba84bf1814e39bbee9c6da8185ed2fb411
Christian Brintnall <christianbrintnall@gmail.com>
2026-05-12 15:58:31 -0700

    add signal for group finish

--- task_group.gd
+++ task_group.gd
@@ -1,6 +1,8 @@
 extends RefCounted
 class_name TaskGroup
 
+signal group_finished
+
 var finished: bool:
   get:
     return _counter <= 0
@@ -11,4 +13,6 @@ func run(callable: Callable):
   _counter += 1
   await callable.call()
   _counter -= 1
+  if _counter == 0:
+    group_finished.emit()
   assert(_counter >= 0)
